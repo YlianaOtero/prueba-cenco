cc74e057980ec82c993386d8ac7e989fd7b0df2b
Yliana <ylianaotero@gmail.com>
2023-10-03 17:43:54 -0300

    Tests

--- terraform/nodes.tf
+++ terraform/nodes.tf
@@ -1,8 +1,30 @@
+resource "aws_security_group" "security_group" {
+  name        = "security-group"
+  description = "Security group for k3s cluster"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+}
+
 resource "aws_instance" "k3s_server" {
   ami           = "ami-03a6eaae9938c858c"
   instance_type = "t2.micro"
+  key_name      = "instance-keys"
 
-  count = 3 
+  security_group_names = [aws_security_group.security_group.name]
+
+  count = 1
 
   tags = {
     Name = "k3s-control-plane-${count.index + 1}"
@@ -12,8 +34,11 @@ resource "aws_instance" "k3s_server" {
 resource "aws_instance" "k3s_worker" {
   ami           = "ami-03a6eaae9938c858c"
   instance_type = "t2.micro"  
+  key_name      = "instance-keys"
 
-  count = 2  
+  security_group_names = [aws_security_group.security_group.name]
+
+  count = 1  
 
   tags = {
     Name = "k3s-worker-${count.index + 1}"
